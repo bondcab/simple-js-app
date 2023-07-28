d195ed173e204747e6502c0f2dd22214633ff841
bondcab <136003551+bondcab@users.noreply.github.com>
2023-07-28 18:44:27 +0100

    Add files via upload
    JavaScript updated with Pokemon list and several Pokemon ojects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,54 @@
-/* stylelint-disable */
 
-let favouriteFood = 'chocolate';
+let pokemonList = (
+    [pokemon1,pokemon2,pokemon3,pokemon4,pokemon5,pokemon6,pokemon7]
+);
 
-document.write(favouriteFood);
 
-alert('Hello world');
+let pokemon1 = {
+    name: "Bulbasaur",
+    height: 2.04,
+    types: [grass, poison]
+
+}
+
+let pokemon2 = {
+    name: "Charmander",
+    height: 2,
+    types: [fire]
+
+}
+
+let pokemon3 = {
+    name: "Squirtle",
+    height: 1.08,
+    types:  [water]
+
+}
+
+let pokemon4 = {
+    name: "Pidgey",
+    height: 1,
+    types: [normal, flying]
+
+}
+
+let pokemon5 = {
+    name: "Rattata",
+    height: 1,
+    types: [normal]
+
+}
+
+let pokemon6 = {
+    name: "Pikachu",
+    height: 1.04,
+    types: [electric]
+
+}
+
+let pokemon7 = {
+    name: "Kigglypuff",
+    height: 1.08,
+    types: [normal, fairy]
+
+}
