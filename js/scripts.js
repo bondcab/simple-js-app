7b5c35180f9b1a6303c21b81b3202572ece7a19a
bondcab <136003551+bondcab@users.noreply.github.com>
2023-08-06 10:07:33 +0100

    exercise_1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,93 @@
-let pokemonList = [
+
+
+let pokemonRepository = (function() {
+
+  let pokemonList = [
     {
     name: "Bulbasaur",
     height: 2.04,
     types: ['grass', 'poison']
 
-},
+    },
 
-{
+   {
     name: "Charmander",
     height: 2,
     types: ['fire']
 
-},
+   },
 
    {
     name: "Squirtle",
     height: 1.08,
     types:  ['water']
 
-},
+   },
 
     {
     name: "Pidgey",
     height: 1,
     types: ['normal', 'flying']
 
-},
+   },
 
     {
     name: "Rattata",
     height: 1,
     types: ['normal']
 
-},
+   },
 
-{
+   {
     name: "Pikachu",
     height: 1.04,
     types: ['electric']
 
-},
-{
+   },
+   {
     name: "Jigglypuff",
     height: 1.08,
     types: ['normal', 'fairy']
 
-},
+   },
    
 ];
 
-//for loop which lists out the name and height of each pokemon
-for (let i = 0; i <= pokemonList.length; i++){
 
-document.write(
-    ("<p>" + pokemonList[i].name) + "</p>" + " Height: " + (pokemonList[i].height)
-)
-
-if (pokemonList[i].height >= 2){
-    document.write(" - Wow, thats big!" )
+function getAll () {
+    return pokemonList;
 }
 
-};
+
+function add(pokemon) {
+    return pokemonList.push(pokemon);
+}
+
+return {
+    getAll: getAll,
+    add: add
+}
 
 
+
+})();
+
+
+
+
+
+
+pokemonRepository.getAll.forEach(listOnSite);
+
+function listOnSite() {
+    document.write(
+        ("<p>" + pokemonList[i].name) + "</p>" + " Height: " + (pokemonList[i].height)
+    )
+    
+    if (get.All.pokemonList[i].height >= 2){
+        document.write(" - Wow, thats big!" )
+    }
+}
 
 
 
