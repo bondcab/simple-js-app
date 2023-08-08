24b3cb02535820aa1d2b611e4476a30785ca6b4a
bondcab <136003551+bondcab@users.noreply.github.com>
2023-08-08 20:27:23 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,151 @@
+// let pokemonRepository = (function () {
+//   let pokemonList = [
+//     {
+//       name: "Bulbasaur",
+//       height: 2.04,
+//       types: ["grass", "poison"],
+//     },
+
+//     {
+//       name: "Charmander",
+//       height: 2,
+//       types: ["fire"],
+//     },
+
+//     {
+//       name: "Squirtle",
+//       height: 1.08,
+//       types: ["water"],
+//     },
+
+//     {
+//       name: "Pidgey",
+//       height: 1,
+//       types: ["normal", "flying"],
+//     },
+
+//     {
+//       name: "Rattata",
+//       height: 1,
+//       types: ["normal"],
+//     },
+
+//     {
+//       name: "Pikachu",
+//       height: 1.04,
+//       types: ["electric"],
+//     },
+//     {
+//       name: "Jigglypuff",
+//       height: 1.08,
+//       types: ["normal", "fairy"],
+//     },
+//   ];
+
+//   function getAll() {
+//     return pokemonList;
+//   }
+
+//   function add(pokemon) {
+//     return pokemonList.push(pokemon);
+//   }
+
+//   return {
+//     getAll: getAll,
+//     add: add,
+//   };
+// })();
+
+// pokemonRepository.getAll().forEach(listOnSite);
+
+// function listOnSite(pokemon) {
+//     let pokemonListClass = document.querySelector('.pokemon-list');
+//   document.write(
+//     "<p>" + pokemonList[pokemon].name + "</p>" + " Height: " + pokemonList[pokemon].height
+//   );
+
+//   if (getAll.pokemonList[pokemon].height >= 2) {
+//     document.write(" - Wow, thats big!");
+//   }
+// }
 
 
-let pokemonRepository = (function() {
 
+
+let pokemonRepository = (function () {
+  
   let pokemonList = [
     {
-    name: "Bulbasaur",
-    height: 2.04,
-    types: ['grass', 'poison']
-
+      name: "Bulbasaur",
+      height: 2.04,
+      types: ["grass", "poison"],
     },
 
-   {
-    name: "Charmander",
-    height: 2,
-    types: ['fire']
-
-   },
-
-   {
-    name: "Squirtle",
-    height: 1.08,
-    types:  ['water']
-
-   },
+    {
+      name: "Charmander",
+      height: 2,
+      types: ["fire"],
+    },
 
     {
-    name: "Pidgey",
-    height: 1,
-    types: ['normal', 'flying']
-
-   },
+      name: "Squirtle",
+      height: 1.08,
+      types: ["water"],
+    },
 
     {
-    name: "Rattata",
-    height: 1,
-    types: ['normal']
+      name: "Pidgey",
+      height: 1,
+      types: ["normal", "flying"],
+    },
 
-   },
+    {
+      name: "Rattata",
+      height: 1,
+      types: ["normal"],
+    },
 
-   {
-    name: "Pikachu",
-    height: 1.04,
-    types: ['electric']
+    {
+      name: "Pikachu",
+      height: 1.04,
+      types: ["electric"],
+    },
+    {
+      name: "Jigglypuff",
+      height: 1.08,
+      types: ["normal", "fairy"],
+    },
+  ];
 
-   },
-   {
-    name: "Jigglypuff",
-    height: 1.08,
-    types: ['normal', 'fairy']
-
-   },
-   
-];
-
-
-function getAll () {
+  function getAll() {
     return pokemonList;
-}
+  }
+
+  function add(pokemon) {
+    if(typeof pokemon === 'object'){
+      return pokemonList.push(pokemon);
+    }
+    
+    
+  }
 
 
-function add(pokemon) {
-    return pokemonList.push(pokemon);
-}
-
-return {
+  return {
     getAll: getAll,
-    add: add
-}
-
-
-
+    add: add,
+  };
 })();
 
+function listOnSite(pokemon) {
+  let pokemonListClass = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('p');
+  listItem.textContent = pokemon.name + " - Height: " + pokemon.height;
 
+  if (pokemon.height >= 2) {
+    listItem.textContent += " - Wow, that's big!";
+  }
 
-
-
-
-pokemonRepository.getAll.forEach(listOnSite);
-
-function listOnSite() {
-    document.write(
-        ("<p>" + pokemonList[i].name) + "</p>" + " Height: " + (pokemonList[i].height)
-    )
-    
-    if (get.All.pokemonList[i].height >= 2){
-        document.write(" - Wow, thats big!" )
-    }
+  pokemonListClass.appendChild(listItem);
 }
 
-
-
-
-
-
-
+pokemonRepository.getAll().forEach(listOnSite);
